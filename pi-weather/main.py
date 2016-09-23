28c226d1ca7d9fbc44cd9ec69524a10f95bbee7c
csnyder <tri413@hotmail>
2016-09-23 12:18:35 -0400

    Initial version

---
+++ pi-weather/main.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+import xml.etree.ElementTree
+import urllib.request
+from datetime import datetime
+from datetime import timedelta
+
+#Settings
+wind_threshold = 6 #notification windspeed
+forecast_days = 5
+
+#Setup
+now = datetime.now()
+begin = now + timedelta(days=1)
+end = now + timedelta(days=forecast_days)
+params = "wspd=wspd"
+
+def dateAsStandardDate(dt: datetime):
+    mm = str(dt.month)
+    dd = str(dt.day)
+    yyyy = str(dt.year)
+    hour = str(dt.hour)
+    mi = str(dt.minute)
+    ss = str(dt.second)
+    return yyyy + "-" + mm + "-" + dd + "T00:00:00"
+
+def fetchNOAAForecast(lat: float, lon: float, begin: datetime, end: datetime, noaaParams: str):
+    noaaUrl = "http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php?lat={0}&lon={1}&product=time-series&begin={2}&end={3}&{4}"
+    formattedUrl = noaaUrl.format(lat, lon, dateAsStandardDate(begin), dateAsStandardDate(end), noaaParams)
+    # NOT returning the correct data
+    forecastXml = urllib.request.urlopen(formattedUrl).read()
+    return forecastXml
+
+def notifyService(maxWind: int, forecasted: str):
+    print("Max wind: " + str(maxWind))
+    iftttUrl = "https://maker.ifttt.com/trigger/windy_soon/with/key/doz4nXqFTYPmsCP3-UtlFw?value1={0}&value2={1}"
+    urllib.request.urlopen(iftttUrl.format(maxWind, forecasted))
+    return
+
+#---------------------------------------------------------------------------
+# BEGINING OF EXECUTION
+noaaForecast = fetchNOAAForecast(37.50, -77.64, begin, end, params)
+rt = xml.etree.ElementTree.fromstring(noaaForecast)
+i = max = maxLoc = 0
+
+# Loop through looking for wind exceeding threshold
+for wind in rt.iter('value'):
+    if int(wind.text) > max:
+        max = int(wind.text)
+        maxLoc = i
+    i += 1
+    
+# Notify service if wind exceeds threshold
+if max > wind_threshold:
+    forecastDate = ""
+    i = 0
+    for dt in rt.iter('start-valid-time'):
+        if i == maxLoc:
+            forecastDate = dt.text
+        i += 1
+    notifyService(max, forecastDate)
